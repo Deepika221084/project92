c8b6cb5e65fa3a5095d3a4902b8afeb576a05227
Deepika221084 <83598331+Deepika221084@users.noreply.github.com>
2021-11-22 19:15:24 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,17 @@
+var score = 0;
+
+function updatescore()
+{
+    score = score+1;
+    document.getElementById("score").innerHTML = "Score:" +score;
+}
+
+function savescore()
+{
+    localStorage.setItem("Score",score);
+}
+
+function nextpage()
+{
+    window.location = "Activity2.html";
+}
